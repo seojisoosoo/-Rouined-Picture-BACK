885b0bbc6953f45f1c6581208d6abbebeb015225
Seojisoo20191941 <amy0480@naver.com>
2022-08-22 23:28:59 +0900

    fix:solved cors error

--- config/settings.py
+++ config/settings.py
@@ -151,5 +151,5 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'https://localhost:3000',
     'http://127.0.0.1:8000',
-    'https://rouined-photo-exhibition.netlify.app/',
+    "https://rouined-photo-exhibition.netlify.app"
 ]
