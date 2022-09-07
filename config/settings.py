6868e1e004b5aba4343482b8f4d3897e13235157
Seojisoo20191941 <amy0480@naver.com>
2022-09-07 16:33:25 +0900

    preparte to deploy

--- config/settings.py
+++ config/settings.py
@@ -20,20 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
+if secret_file==True:
+    with open(secret_file) as f:
+        secrets = json.loads(f.read())
 
-def get_secret(setting):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+    def get_secret(setting):
+        """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+        try:
+            return secrets[setting]
+        except KeyError:
+            error_msg = "Set the {} environment variable".format(setting)
+            raise ImproperlyConfigured(error_msg)
 
 
-SECRET_KEY = get_secret("SECRET_KEY")
+    SECRET_KEY = get_secret("SECRET_KEY")
 
+else:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -159,3 +162,7 @@ CORS_ORIGIN_WHITELIST = [
     "https://rouined-photo-exhibition.netlify.app"
 ]
 django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
